3495179995ef512abe8d2193ddf3e59234f738b2
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-24 08:42:36 +0500

    add date in program

--- 45 qustion/10Adding Comments/index.ts
+++ 45 qustion/10Adding Comments/index.ts
@@ -1,8 +1,8 @@
-//addition two number in this code
+//zubairkhan 23-03-2024  addition two number in this code
 let a =10;
-let b = 20
+let b = 20;
 console.log(a+b);
-//print a name with uppercase & lowercase
+//print a name with uppercase & lowercase 
 let myname= "zubair ahmed";
 console.log(myname.toUpperCase());
 console.log(myname.charAt(0).toUpperCase()+ myname.slice(1).toLowerCase())
