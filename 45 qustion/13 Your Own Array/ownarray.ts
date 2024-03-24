1d705340aa76826c0a522f1107091a25ee08848d
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-24 09:38:21 +0500

    answer

---
+++ 45 qustion/13 Your Own Array/ownarray.ts
@@ -0,0 +1,5 @@
+let favoriteTransport=["motocycle125 Honda","suzuki swift","car ciaz","motorbike honda"
+,"bike yamaha","car toyota","kia sportage","car civic honda",]
+let massage = "I would like to own a ~ ";
+for(let i=0;i<favoriteTransport.length;i++){console.log(massage+favoriteTransport[i])}
+
