928fcbbfdaedc587e757cfbb92d46979a0364517
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-24 09:17:29 +0500

    greeting

---
+++ 45 qustion/12 Greeting/greeting.js
@@ -0,0 +1,6 @@
+"use strict";
+let friendsName = ["ahmed", "mohamed", "ali", "omar", "sohaib", "ahmed", "abu", "essa",];
+let massage = "Hello ";
+for (let i = 0; i < friendsName.length; i++) {
+    console.log(massage + friendsName[i]);
+}
---
+++ 45 qustion/12 Greeting/greeting.ts
@@ -0,0 +1,3 @@
+let friendsName =["ahmed","mohamed","ali","omar","sohaib","ahmed","abu","essa",];
+let massage = "Hello ";
+for (let i = 0; i < friendsName.length; i++) {console.log(massage + friendsName[i])}
