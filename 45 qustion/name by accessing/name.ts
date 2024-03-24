e45662abfd43cfe8c79af2806f0129af00291839
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-24 08:54:33 +0500

    work

---
+++ 45 qustion/name by accessing/name.ts
@@ -0,0 +1,11 @@
+let names =["ali","muhammad","asad","saad","zain","zaid","zubair","zahid","zainab",]
+//for(let i=0;i<names.length;i++){console.log(names[i])};
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
+console.log(names[6]);
+console.log(names[7]);
+console.log(names[8]);
