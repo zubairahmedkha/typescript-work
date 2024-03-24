b09cf19312be7c7f42376a09937bfbc5f49da4ed
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-24 15:06:27 +0500

    coding

---
+++ 45 qustion/15 Changing Guest List/guest.js
@@ -0,0 +1,12 @@
+"use strict";
+let guest = ["Irshad", "Rashid", "Peer Muhammad", "Essa khan"];
+for (let i = 0; i < guest.length; i++) {
+    console.log(`\nDear ${guest[i]}, you are invited to dinner at my
+ house.`);
+}
+console.log("\t\n Irshad can't make it to dinner, Muhammad Ali invited ");
+let guest1 = ["Muhammad Ali", "Rashid", "Peer Muhammad", "Essa khan"];
+for (let i = 0; i < guest1.length; i++) {
+    console.log(`\n Dear ${guest1[i]}, you are invited to dinner at my
+ house.`);
+}
