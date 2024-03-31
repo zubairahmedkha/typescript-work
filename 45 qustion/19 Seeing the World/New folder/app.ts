a26ed12b9e19c9fc7d282022dd5ed802e1d26c34
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-03-31 11:22:33 +0500

    work

---
+++ 45 qustion/19 Seeing the World/New folder/app.ts
@@ -0,0 +1,16 @@
+let fivePlaces =["newyork","london","paris",
+"dubai","makkah"]
+console.log("original order:",fivePlaces)
+console.log("Alphabatical order:",fivePlaces.sort())
+console.log(" still in origenal ordre:",fivePlaces)
+console.log("reverse order",fivePlaces.reverse())
+console.log("still in origenal order",fivePlaces)
+ fivePlaces.reverse()
+ console.log("reverse order", fivePlaces)
+ console.log( fivePlaces.sort() )
+ fivePlaces.sort()
+ console.log("Alphabatical order", fivePlaces)
+ console.log("reverse order", fivePlaces.reverse())
+
+
+
