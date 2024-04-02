ccdf9aabd06b119706ec30ad0eed69d1124c88bc
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-04-02 11:21:30 +0500

    coding

---
+++ 45 qustion/21 Type script Object/object.js
@@ -0,0 +1,9 @@
+"use strict";
+let objectList = {
+    "mountain": ["K2", "Evrest", "KHenjhar"],
+    "Rivers": ["Nile", "Ganga", "Yamuna"],
+    "Country": ["India", "Pakistan", "Bangladesh"],
+    "City": ["Delhi", "Mumbai", "Kolkata"],
+    "Foods": ["Pizza", "Burger", "Pasta"]
+};
+console.log(objectList);
---
+++ 45 qustion/21 Type script Object/object.ts
@@ -0,0 +1,9 @@
+let objectList ={
+    "mountain":["K2","Evrest","KHenjhar"],
+    "Rivers":["Nile","Ganga","Yamuna"],
+    "Country":["India","Pakistan","Bangladesh"],
+    "City":["Delhi","Mumbai","Kolkata"],
+    "Foods":["Pizza","Burger","Pasta"]
+
+}
+console.log(objectList);
