9fc00feef1a9550228767a8a9375d6df2a2de8d2
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-04-04 19:43:21 +0500

    to do update

---
+++ 45 qustion/24 Conditional Tests/index.ts
@@ -0,0 +1,5 @@
+//Expand your conditional tests to cover a wider range of comparisons
+
+let car = "swift"
+
+
---
+++ project 05 To do/todo/todos.ts
@@ -0,0 +1,57 @@
+#! /usr/bin/env node
+console.clear();
+import inquirer from "inquirer";
+let todo =[]
+let condition = true
+
+while (condition){
+
+let  todosQuestion =await inquirer.prompt([{
+    name:"q1",
+    message:" what would like to you want to add in list",
+     type:"inupt"},
+     
+     {
+        name:"q2",
+        message:"do you want to add more in list",
+        type:"list",
+        choices:["update","Exit"],
+        //  default:true
+    },
+   ]);
+   // if condition is true then it will add in list
+  if (todosQuestion.q2 === "update" )  
+  {condition = true
+
+
+  } ;
+  if (todosQuestion.q2 === "Exit"){ 
+    todo.push(todosQuestion.q1)
+    console.log(todo)
+  
+  
+    break;
+}
+    
+
+  
+  
+  
+
+    todo.push(todosQuestion.q1)
+    
+condition = todosQuestion.q2
+    console.log(todo)}
+    // write a code of currency converter here?
+    
+
+
+    
+
+
+
+
+
+
+
+
