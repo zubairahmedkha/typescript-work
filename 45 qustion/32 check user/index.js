5ef3e3167b3671f49a5ec4522f91ee275daa314c
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-04-29 19:51:31 +0500

    work

--- 45 qustion/32 check user/index.js
+++ 45 qustion/32 check user/index.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let current_user = ["aric", "essa", "khan", "roshan", "ajmal"];
 let newUsers = ["imran", "aric", "essa", "Ali", "hmaza"];
+function checkUserNameAvailablity(userName) {
+    let userNameTocheck = userName.toLowerCase();
+}
 if (current_user == newUsers) {
     console.log("These are user are in the list");
 }
